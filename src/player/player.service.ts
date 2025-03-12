a682541f97bbac6ce4ce5ad6b3082ebf68167ff9
Dao Manh <superhero21442922@gmail.com>
2025-03-12 10:41:55 +0700

    push-code-12/3

--- src/player/player.service.ts
+++ src/player/player.service.ts
@@ -1,3 +1,4 @@
+// src/player/player.service.ts
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Player } from './entities/player.entity';
@@ -10,7 +11,7 @@ export class PlayerService {
     private playerRepository: Repository<Player>,
   ) {}
 
-  async addPlayer(name?: string): Promise<Player> {
+  async addPlayer(name: string): Promise<Player> {
     const player = this.playerRepository.create({
       name,
       score: 0,
@@ -27,24 +28,22 @@ export class PlayerService {
     return this.playerRepository.find();
   }
 
-  async getPlayerById(id: string): Promise<void> {
-    await this.playerRepository.findOne({ where: { id } });
+  async getPlayerById(id: string): Promise<Player> {
+    const player = await this.playerRepository.findOne({ where: { id } });
+    if (!player) throw new Error("Player not found");
+    return player;
   }
 
   async resetScore(id: string): Promise<Player> {
-    const player = await this.playerRepository.findOne({ where: { id } });
-    if (!player) throw new Error('Player not found');
+    const player = await this.getPlayerById(id);
     player.score = 0;
     return this.playerRepository.save(player);
   }
 
-  async updateScore(id: string, score: number): Promise<Player> {
-    const player = await this.playerRepository.findOne({ where: { id } });
-    if (!player) {
-      throw new Error('Player not found');
-    }
-    player.score = (player.score || 0) + score;
-    console.log(player.score);
+  async updateScore(id: string, scoreDelta: number): Promise<Player> {
+    const player = await this.getPlayerById(id);
+    player.score = (player.score || 0) + scoreDelta;
+    // console.log("New score:", player.score);
     return this.playerRepository.save(player);
   }
 
